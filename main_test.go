ef3a334fcd0d7e99306db116705cb7273b94ab5f
Scott Ferguson <scottwferg@gmail.com>
2013-12-09 09:46:53 -0600

    Add resize benchmark to test suite

---
+++ main_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	. "launchpad.net/gocheck"
+	"testing"
+)
+
+// Hook up gocheck into the "go test" runner.
+func Test(t *testing.T) {
+	TestingT(t)
+}
+
+func setUpSuite(c *C) {}
+
+func setUpTest(c *C) {}
+
+func tearDownTest(c *C) {}
+
+func tearDownSuite(c *C) {}
---
+++ resize_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bytes"
+	"github.com/vokalinteractive/vip/fetch"
+	"io/ioutil"
+	. "launchpad.net/gocheck"
+)
+
+var (
+	_ = Suite(&ResizeSuite{})
+)
+
+type ResizeSuite struct{}
+
+func (s *ResizeSuite) BenchmarkResize(c *C) {
+	file, err := ioutil.ReadFile("test/AWESOME.jpg")
+	c.Assert(err, IsNil)
+
+	ctx := &fetch.CacheContext{
+		Width: 720,
+	}
+
+	for i := 0; i < c.N; i++ {
+		// Need a new io.Reader on every iteration
+		buf := bytes.NewBuffer(file)
+		_, err := fetch.Resize(buf, ctx)
+		c.Assert(err, IsNil)
+	}
+}
