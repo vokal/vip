a0b521c785ac2215c7582035ecf57111f279f735
Scott Ferguson <scottwferg@gmail.com>
2013-12-09 11:32:11 -0600

    Add thumbnail benchmark

--- resize_test.go
+++ resize_test.go
@@ -13,7 +13,23 @@ var (
 
 type ResizeSuite struct{}
 
-func (s *ResizeSuite) BenchmarkResize(c *C) {
+func (s *ResizeSuite) BenchmarkThumbnailResize(c *C) {
+	file, err := ioutil.ReadFile("test/AWESOME.jpg")
+	c.Assert(err, IsNil)
+
+	ctx := &fetch.CacheContext{
+		Width: 160,
+	}
+
+	for i := 0; i < c.N; i++ {
+		// Need a new io.Reader on every iteration
+		buf := bytes.NewBuffer(file)
+		_, err := fetch.Resize(buf, ctx)
+		c.Assert(err, IsNil)
+	}
+}
+
+func (s *ResizeSuite) BenchmarkLargeResize(c *C) {
 	file, err := ioutil.ReadFile("test/AWESOME.jpg")
 	c.Assert(err, IsNil)
 
