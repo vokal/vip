a5c166f21c62134573ff679c3b9db999f1755b53
Scott Ferguson <scottwferg@gmail.com>
2014-09-02 12:49:33 -0500

    Add center cropping functionality

--- fetch/context.go
+++ fetch/context.go
@@ -12,6 +12,7 @@ type CacheContext struct {
 	ImageId string
 	Bucket  string
 	Width   int
+	Crop    bool
 }
 
 func (c *CacheContext) ReadOriginal(s store.ImageStore) (io.ReadCloser, error) {
@@ -24,7 +25,7 @@ func (c *CacheContext) ReadOriginal(s store.ImageStore) (io.ReadCloser, error) {
 	return r, err
 }
 
-func (c *CacheContext) ReadResized(s store.ImageStore) (io.ReadCloser, error) {
+func (c *CacheContext) ReadModified(s store.ImageStore) (io.ReadCloser, error) {
 	data, err := s.GetReader(c.Bucket, c.CacheKey())
 	if err != nil {
 		log.Printf("s3 download: %s", err.Error())
@@ -34,14 +35,17 @@ func (c *CacheContext) ReadResized(s store.ImageStore) (io.ReadCloser, error) {
 	return data, err
 }
 
-func (c *CacheContext) WriteResized(buf []byte, s store.ImageStore) error {
+func (c *CacheContext) WriteModified(buf []byte, s store.ImageStore) error {
 	return s.Put(c.Bucket, c.CacheKey(), buf, http.DetectContentType(buf))
 }
 
 func (c *CacheContext) CacheKey() string {
-	if c.Width == 0 {
-		return fmt.Sprintf("%s", c.ImageId)
+	switch {
+	case c.Crop && c.Width != 0:
+		return fmt.Sprintf("%s/c/s/%d", c.ImageId, c.Width)
+	case c.Width != 0:
+		return fmt.Sprintf("%s/s/%d", c.ImageId, c.Width)
 	}
 
-	return fmt.Sprintf("%s/s/%d", c.ImageId, c.Width)
+	return fmt.Sprintf("%s", c.ImageId)
 }
