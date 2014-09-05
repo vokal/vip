804cf74b8d0ed1edf118abef533a2de033fd10a5
Alex Nimmer <alexnimmer@VOKALs-MacBook-Air.local>
2014-09-05 16:29:00 -0500

    add region support

--- region_test.go
+++ region_test.go
@@ -1,30 +1,28 @@
 package main
 
 import (
-    "testing"
-    "os"
-    "launchpad.net/goamz/aws"
-    "vip/main"
+	"os"
+	"testing"
 )
 
 func TestRegion(t *testing.T) {
-    regionName =  "us-west-2"
-    os.Setenv("AWS_REGION", regionName)
-    region = getRegion()
-    if region.Name == regionName {
-        t.log('Region test passed')
-    } else {
-        t.Error("Region test failed: expected region %s recieved region %s", regionName, region.Name)
-    }
+	regionName := "us-west-2"
+	os.Setenv("AWS_REGION", regionName)
+	region := getRegion()
+	if region.Name == regionName {
+		t.Log("Region test passed")
+	} else {
+		t.Error("Region test failed: expected region %s recieved region %s", regionName, region.Name)
+	}
 }
 
-func TestRegionDefault(t *testing T) {
-    regionName = "blarg"
-    os.Setenv("AWS_REGION", regionName)
-    region = getRegion()
-    if region.Name = "us-east-1"{
-        t.log("Default Region test passed")
-    } else {
-        t.Error("Default Region test failed: default region us-east-1 not returned")
-    }
+func TestRegionDefault(t *testing.T) {
+	regionName := "blarg"
+	os.Setenv("AWS_REGION", regionName)
+	region := getRegion()
+	if region.Name == "us-east-1" {
+		t.Log("Default Region test passed")
+	} else {
+		t.Error("Default Region test failed: default region us-east-1 not returned")
+	}
 }
