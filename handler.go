6af981c7aa9305ace8042233d2f9e8c4a1001d89
MichaelButkovic <michael.bukovic@gmail.com>
2014-10-24 16:17:56 -0500

    set domain via env variable

--- handler.go
+++ handler.go
@@ -24,20 +24,16 @@ type verifyAuth func(http.ResponseWriter, *http.Request)
 func (h verifyAuth) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// Enable cross-origin requests
 	// TODO: Whitelist the domain via an environment variable
-	if origin := r.Header.Get("Origin"); origin != "" {
-		w.Header().Set("Access-Control-Allow-Origin", origin)
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers",
-			"Accept, Content-Type, Content-Length, Accept-Encoding, X-Vip-Token, Authorization")
+	if domain := os.Getenv("DOMAIN_DATA"); domain != "": {
+		if origin := r.Header.Get("Origin"); origin != "" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+			w.Header().Set("Access-Control-Allow-Headers",
+				"Accept, Content-Type, Content-Length, Accept-Encoding, X-Vip-Token, Authorization")
+		}
 	}
 
 	if r.Method == "OPTIONS" {
-		return
-	}
-
-	auth := r.Header.Get("X-Vip-Token")
-	if auth != authToken {
-		w.WriteHeader(http.StatusUnauthorized)
 		return
 	}
 
