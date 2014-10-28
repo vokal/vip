1925f6c50fbaf21753f3fa0b5b258cd793d94fc9
MichaelButkovic <michael.bukovic@gmail.com>
2014-10-28 12:51:35 -0500

    Whitelist the domain via an environment variable

--- handler.go
+++ handler.go
@@ -23,15 +23,20 @@ type verifyAuth func(http.ResponseWriter, *http.Request)
 
 func (h verifyAuth) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// Enable cross-origin requests
-	// TODO: Whitelist the domain via an environment variable
-	if domain := os.Getenv("DOMAIN_DATA"); domain == "": {
+	if domain := os.Getenv("DOMAIN_DATA"); domain == "" {
 		if origin := r.Header.Get("Origin"); origin != "" {
 			w.Header().Set("Access-Control-Allow-Origin", origin)
 			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 			w.Header().Set("Access-Control-Allow-Headers",
 				"Accept, Content-Type, Content-Length, Accept-Encoding, X-Vip-Token, Authorization")
 		}
-	}
+
+		auth := r.Header.Get("X-Vip-Token")
+		if auth != authToken {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}	
+	}	
 
 	if r.Method == "OPTIONS" {
 		return
