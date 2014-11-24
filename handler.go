1cb5ac3b0573e614cb0e0bb8af0e9573fc5eef37
Alex Nimmer <alexnimmer@gmail.com>
2014-11-24 15:59:15 -0600

    removed unused flag

--- handler.go
+++ handler.go
@@ -122,7 +122,7 @@ func handleUpload(w http.ResponseWriter, r *http.Request) {
 
 	if r.URL.Host == "" {
 		uri.Host = os.Getenv("URI_HOSTNAME")
-		if *secure {
+		if key != "" && cert != "" {
 			uri.Scheme = "https"
 		} else {
 			uri.Scheme = "http"
