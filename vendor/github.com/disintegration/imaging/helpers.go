4224faad21deb12322b29d9204a946fd1a9d5575
Scott Ferguson <scottwferg@gmail.com>
2016-02-25 11:48:05 -0600

    Updated imaging package

--- vendor/github.com/disintegration/imaging/helpers.go
+++ vendor/github.com/disintegration/imaging/helpers.go
@@ -1,61 +1,85 @@
+/*
+Package imaging provides basic image manipulation functions (resize, rotate, flip, crop, etc.).
+This package is based on the standard Go image package and works best along with it.
+
+Image manipulation functions provided by the package take any image type
+that implements `image.Image` interface as an input, and return a new image of
+`*image.NRGBA` type (32bit RGBA colors, not premultiplied by alpha).
+*/
 package imaging
 
 import (
-	"fmt"
+	"errors"
 	"image"
 	"image/color"
-	_ "image/gif"
+	"image/gif"
 	"image/jpeg"
 	"image/png"
-	"math"
+	"io"
 	"os"
 	"path/filepath"
 	"strings"
 
-	"code.google.com/p/go.image/bmp"
-	"code.google.com/p/go.image/tiff"
+	"golang.org/x/image/bmp"
+	"golang.org/x/image/tiff"
 )
 
-// Open loads an image from file
-func Open(filename string) (img image.Image, err error) {
-	file, err := os.Open(filename)
-	if err != nil {
-		return
-	}
-	defer file.Close()
+type Format int
 
-	img, _, err = image.Decode(file)
-	if err != nil {
-		return
-	}
+const (
+	JPEG Format = iota
+	PNG
+	GIF
+	TIFF
+	BMP
+)
 
-	img = toNRGBA(img)
-	return
+func (f Format) String() string {
+	switch f {
+	case JPEG:
+		return "JPEG"
+	case PNG:
+		return "PNG"
+	case GIF:
+		return "GIF"
+	case TIFF:
+		return "TIFF"
+	case BMP:
+		return "BMP"
+	default:
+		return "Unsupported"
+	}
 }
 
-// Save saves the image to file with the specified filename.
-// The format is determined from the filename extension: "jpg" (or "jpeg"), "png", "tif" (or "tiff") and "bmp" are supported.
-func Save(img image.Image, filename string) (err error) {
-	format := strings.ToLower(filepath.Ext(filename))
-	okay := false
-	for _, ext := range []string{".jpg", ".jpeg", ".png", ".tif", ".tiff", ".bmp"} {
-		if format == ext {
-			okay = true
-			break
-		}
-	}
-	if !okay {
-		return fmt.Errorf(`imaging: unsupported image format: "%s"`, format)
-	}
+var (
+	ErrUnsupportedFormat = errors.New("imaging: unsupported image format")
+)
 
-	file, err := os.Create(filename)
+// Decode reads an image from r.
+func Decode(r io.Reader) (image.Image, error) {
+	img, _, err := image.Decode(r)
 	if err != nil {
-		return
+		return nil, err
+	}
+	return toNRGBA(img), nil
+}
+
+// Open loads an image from file
+func Open(filename string) (image.Image, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
 	}
 	defer file.Close()
+	img, err := Decode(file)
+	return img, err
+}
 
+// Encode writes the image img to w in the specified format (JPEG, PNG, GIF, TIFF or BMP).
+func Encode(w io.Writer, img image.Image, format Format) error {
+	var err error
 	switch format {
-	case ".jpg", ".jpeg":
+	case JPEG:
 		var rgba *image.RGBA
 		if nrgba, ok := img.(*image.NRGBA); ok {
 			if nrgba.Opaque() {
@@ -67,19 +91,51 @@ func Save(img image.Image, filename string) (err error) {
 			}
 		}
 		if rgba != nil {
-			err = jpeg.Encode(file, rgba, &jpeg.Options{Quality: 95})
+			err = jpeg.Encode(w, rgba, &jpeg.Options{Quality: 95})
 		} else {
-			err = jpeg.Encode(file, img, &jpeg.Options{Quality: 95})
+			err = jpeg.Encode(w, img, &jpeg.Options{Quality: 95})
 		}
 
-	case ".png":
-		err = png.Encode(file, img)
-	case ".tif", ".tiff":
-		err = tiff.Encode(file, img, &tiff.Options{Compression: tiff.Deflate, Predictor: true})
-	case ".bmp":
-		err = bmp.Encode(file, img)
+	case PNG:
+		err = png.Encode(w, img)
+	case GIF:
+		err = gif.Encode(w, img, &gif.Options{NumColors: 256})
+	case TIFF:
+		err = tiff.Encode(w, img, &tiff.Options{Compression: tiff.Deflate, Predictor: true})
+	case BMP:
+		err = bmp.Encode(w, img)
+	default:
+		err = ErrUnsupportedFormat
 	}
-	return
+	return err
+}
+
+// Save saves the image to file with the specified filename.
+// The format is determined from the filename extension: "jpg" (or "jpeg"), "png", "gif", "tif" (or "tiff") and "bmp" are supported.
+func Save(img image.Image, filename string) (err error) {
+	formats := map[string]Format{
+		".jpg":  JPEG,
+		".jpeg": JPEG,
+		".png":  PNG,
+		".tif":  TIFF,
+		".tiff": TIFF,
+		".bmp":  BMP,
+		".gif":  GIF,
+	}
+
+	ext := strings.ToLower(filepath.Ext(filename))
+	f, ok := formats[ext]
+	if !ok {
+		return ErrUnsupportedFormat
+	}
+
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	return Encode(file, img, f)
 }
 
 // New creates a new image with the specified width and height, and fills it with the specified color.
@@ -109,6 +165,229 @@ func New(width, height int, fillColor color.Color) *image.NRGBA {
 	return dst
 }
 
+// Clone returns a copy of the given image.
+func Clone(img image.Image) *image.NRGBA {
+	srcBounds := img.Bounds()
+	srcMinX := srcBounds.Min.X
+	srcMinY := srcBounds.Min.Y
+
+	dstBounds := srcBounds.Sub(srcBounds.Min)
+	dstW := dstBounds.Dx()
+	dstH := dstBounds.Dy()
+	dst := image.NewNRGBA(dstBounds)
+
+	switch src := img.(type) {
+
+	case *image.NRGBA:
+		rowSize := srcBounds.Dx() * 4
+		parallel(dstH, func(partStart, partEnd int) {
+			for dstY := partStart; dstY < partEnd; dstY++ {
+				di := dst.PixOffset(0, dstY)
+				si := src.PixOffset(srcMinX, srcMinY+dstY)
+				copy(dst.Pix[di:di+rowSize], src.Pix[si:si+rowSize])
+			}
+		})
+
+	case *image.NRGBA64:
+		parallel(dstH, func(partStart, partEnd int) {
+			for dstY := partStart; dstY < partEnd; dstY++ {
+				di := dst.PixOffset(0, dstY)
+				si := src.PixOffset(srcMinX, srcMinY+dstY)
+				for dstX := 0; dstX < dstW; dstX++ {
+
+					dst.Pix[di+0] = src.Pix[si+0]
+					dst.Pix[di+1] = src.Pix[si+2]
+					dst.Pix[di+2] = src.Pix[si+4]
+					dst.Pix[di+3] = src.Pix[si+6]
+
+					di += 4
+					si += 8
+
+				}
+			}
+		})
+
+	case *image.RGBA:
+		parallel(dstH, func(partStart, partEnd int) {
+			for dstY := partStart; dstY < partEnd; dstY++ {
+				di := dst.PixOffset(0, dstY)
+				si := src.PixOffset(srcMinX, srcMinY+dstY)
+				for dstX := 0; dstX < dstW; dstX++ {
+
+					a := src.Pix[si+3]
+					dst.Pix[di+3] = a
+					switch a {
+					case 0:
+						dst.Pix[di+0] = 0
+						dst.Pix[di+1] = 0
+						dst.Pix[di+2] = 0
+					case 0xff:
+						dst.Pix[di+0] = src.Pix[si+0]
+						dst.Pix[di+1] = src.Pix[si+1]
+						dst.Pix[di+2] = src.Pix[si+2]
+					default:
+						var tmp uint16
+						tmp = uint16(src.Pix[si+0]) * 0xff / uint16(a)
+						dst.Pix[di+0] = uint8(tmp)
+						tmp = uint16(src.Pix[si+1]) * 0xff / uint16(a)
+						dst.Pix[di+1] = uint8(tmp)
+						tmp = uint16(src.Pix[si+2]) * 0xff / uint16(a)
+						dst.Pix[di+2] = uint8(tmp)
+					}
+
+					di += 4
+					si += 4
+
+				}
+			}
+		})
+
+	case *image.RGBA64:
+		parallel(dstH, func(partStart, partEnd int) {
+			for dstY := partStart; dstY < partEnd; dstY++ {
+				di := dst.PixOffset(0, dstY)
+				si := src.PixOffset(srcMinX, srcMinY+dstY)
+				for dstX := 0; dstX < dstW; dstX++ {
+
+					a := src.Pix[si+6]
+					dst.Pix[di+3] = a
+					switch a {
+					case 0:
+						dst.Pix[di+0] = 0
+						dst.Pix[di+1] = 0
+						dst.Pix[di+2] = 0
+					case 0xff:
+						dst.Pix[di+0] = src.Pix[si+0]
+						dst.Pix[di+1] = src.Pix[si+2]
+						dst.Pix[di+2] = src.Pix[si+4]
+					default:
+						var tmp uint16
+						tmp = uint16(src.Pix[si+0]) * 0xff / uint16(a)
+						dst.Pix[di+0] = uint8(tmp)
+						tmp = uint16(src.Pix[si+2]) * 0xff / uint16(a)
+						dst.Pix[di+1] = uint8(tmp)
+						tmp = uint16(src.Pix[si+4]) * 0xff / uint16(a)
+						dst.Pix[di+2] = uint8(tmp)
+					}
+
+					di += 4
+					si += 8
+
+				}
+			}
+		})
+
+	case *image.Gray:
+		parallel(dstH, func(partStart, partEnd int) {
+			for dstY := partStart; dstY < partEnd; dstY++ {
+				di := dst.PixOffset(0, dstY)
+				si := src.PixOffset(srcMinX, srcMinY+dstY)
+				for dstX := 0; dstX < dstW; dstX++ {
+
+					c := src.Pix[si]
+					dst.Pix[di+0] = c
+					dst.Pix[di+1] = c
+					dst.Pix[di+2] = c
+					dst.Pix[di+3] = 0xff
+
+					di += 4
+					si += 1
+
+				}
+			}
+		})
+
+	case *image.Gray16:
+		parallel(dstH, func(partStart, partEnd int) {
+			for dstY := partStart; dstY < partEnd; dstY++ {
+				di := dst.PixOffset(0, dstY)
+				si := src.PixOffset(srcMinX, srcMinY+dstY)
+				for dstX := 0; dstX < dstW; dstX++ {
+
+					c := src.Pix[si]
+					dst.Pix[di+0] = c
+					dst.Pix[di+1] = c
+					dst.Pix[di+2] = c
+					dst.Pix[di+3] = 0xff
+
+					di += 4
+					si += 2
+
+				}
+			}
+		})
+
+	case *image.YCbCr:
+		parallel(dstH, func(partStart, partEnd int) {
+			for dstY := partStart; dstY < partEnd; dstY++ {
+				di := dst.PixOffset(0, dstY)
+				for dstX := 0; dstX < dstW; dstX++ {
+
+					srcX := srcMinX + dstX
+					srcY := srcMinY + dstY
+					siy := src.YOffset(srcX, srcY)
+					sic := src.COffset(srcX, srcY)
+					r, g, b := color.YCbCrToRGB(src.Y[siy], src.Cb[sic], src.Cr[sic])
+					dst.Pix[di+0] = r
+					dst.Pix[di+1] = g
+					dst.Pix[di+2] = b
+					dst.Pix[di+3] = 0xff
+
+					di += 4
+
+				}
+			}
+		})
+
+	case *image.Paletted:
+		plen := len(src.Palette)
+		pnew := make([]color.NRGBA, plen)
+		for i := 0; i < plen; i++ {
+			pnew[i] = color.NRGBAModel.Convert(src.Palette[i]).(color.NRGBA)
+		}
+
+		parallel(dstH, func(partStart, partEnd int) {
+			for dstY := partStart; dstY < partEnd; dstY++ {
+				di := dst.PixOffset(0, dstY)
+				si := src.PixOffset(srcMinX, srcMinY+dstY)
+				for dstX := 0; dstX < dstW; dstX++ {
+
+					c := pnew[src.Pix[si]]
+					dst.Pix[di+0] = c.R
+					dst.Pix[di+1] = c.G
+					dst.Pix[di+2] = c.B
+					dst.Pix[di+3] = c.A
+
+					di += 4
+					si += 1
+
+				}
+			}
+		})
+
+	default:
+		parallel(dstH, func(partStart, partEnd int) {
+			for dstY := partStart; dstY < partEnd; dstY++ {
+				di := dst.PixOffset(0, dstY)
+				for dstX := 0; dstX < dstW; dstX++ {
+
+					c := color.NRGBAModel.Convert(img.At(srcMinX+dstX, srcMinY+dstY)).(color.NRGBA)
+					dst.Pix[di+0] = c.R
+					dst.Pix[di+1] = c.G
+					dst.Pix[di+2] = c.B
+					dst.Pix[di+3] = c.A
+
+					di += 4
+
+				}
+			}
+		})
+
+	}
+
+	return dst
+}
+
 // This function used internally to convert any image type to NRGBA if needed.
 func toNRGBA(img image.Image) *image.NRGBA {
 	srcBounds := img.Bounds()
@@ -118,9 +397,4 @@ func toNRGBA(img image.Image) *image.NRGBA {
 		}
 	}
 	return Clone(img)
-}
-
-// clamp & round float64 to uint8 (0..255)
-func clamp(v float64) uint8 {
-	return uint8(math.Min(math.Max(v, 0.0), 255.0) + 0.5)
 }
